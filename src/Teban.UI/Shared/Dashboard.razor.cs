706d51a033fd40b3fd47516db9c149e8ea6bd845
eruvalca <eruvalca@outlook.com>
2022-10-15 17:03:16 -0400

    - created basic dashboard to route to create new budget

---
+++ src/Teban.UI/Shared/Dashboard.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Teban.Domain.Entities;
+using Teban.UI.Services;
+
+namespace Teban.UI.Shared
+{
+    [Authorize]
+    public partial class Dashboard
+    {
+        [Inject]
+        private BudgetsService BudgetsService { get; set; }
+
+        private List<Budget>? Budgets { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            var budgetsRequest = await BudgetsService.GetBudgets();
+
+            if (budgetsRequest.Succeeded)
+            {
+                Budgets = budgetsRequest.Data?.ToList();
+            }
+        }
+    }
+}

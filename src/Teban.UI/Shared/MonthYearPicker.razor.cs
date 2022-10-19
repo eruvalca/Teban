0d7e1d92898961b3baceeedacc18382678fdc4b3
eruvalca <eruvalca@outlook.com>
2022-10-19 15:05:53 -0400

    - created basic month year picker component

---
+++ src/Teban.UI/Shared/MonthYearPicker.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teban.UI.Shared
+{
+    public partial class MonthYearPicker
+    {
+        [Parameter]
+        public DateTime SelectedMonthYear { get; set; }
+        [Parameter]
+        public EventCallback<int> MonthYearCallback { get; set; }
+    }
+}

27b72943da702158b29cfe76f4192922db91ad42
eruvalca <eruvalca@outlook.com>
2023-04-11 22:56:11 -0400

    - more ui changes and bday updates

--- Teban.Contracts/Helpers/V1/DateTimeExtensions.cs
+++ Teban.Contracts/Helpers/V1/DateTimeExtensions.cs
@@ -8,4 +8,11 @@ public static class DateTimeExtensions
         if (dateOfBirth.Date > today.AddYears(-age)) age--;
         return age;
     }
+
+    public static int GetAgeAtDate(this DateTime dateOfBirth, DateTime atDate)
+    {
+        var age = atDate.Year - dateOfBirth.Year;
+        if (dateOfBirth.Date > atDate.AddYears(-age)) age--;
+        return age;
+    }
 }
